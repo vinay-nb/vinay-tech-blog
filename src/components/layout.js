bef97fc2d1f4f4debaee76754e6c7ce44e8e643b
Vinay Bhat <vinaybhathg11@gmail.com>
2021-01-22 17:11:27 +0530

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        Made With ❤️ by
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://www.linkedin.com/in/vinay-nb">Vinay Bhat</a>
       </footer>
     </div>
   )
